966bfc1aee19a9eba8883047610edd7afc4865d5
Dearmann <duszak0@gmail.com>
2022-11-22 14:41:28 +0100

    game-list init

---
+++ src/app/game-list/game.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GameResponse } from './GameResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  private readonly backendUrl = "http://localhost:8080/games";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllGames(): Observable<GameResponse[]> {
+    return this.httpClient.get<GameResponse[]>(this.backendUrl);
+  }
+}
