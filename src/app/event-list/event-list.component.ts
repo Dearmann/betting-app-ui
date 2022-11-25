62c5379bd3c0a25a2c13baaa21c2907672d29af3
Dearmann <duszak0@gmail.com>
2022-11-25 17:02:51 +0100

    Fill list with events

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GameService } from '../game-list/game.service';
+import { GameResponse } from '../game-list/GameResponse';
+import { EventService } from './event.service';
+import { EventResponse } from './EventResponse';
 
 @Component({
   selector: 'app-event-list',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
-  constructor() { }
+  public events: EventResponse[] = [];
+  public game!: GameResponse;
+  public gameId: number;
+  public error: any;
+
+  constructor(
+    private readonly eventService: EventService,
+    private readonly route: ActivatedRoute,
+    private readonly gameService: GameService
+  ) {
+    this.gameId = parseInt(this.route.snapshot.paramMap.get('gameId') || "");
+  }
 
   ngOnInit(): void {
+    this.getEventsByGameId(this.gameId);
+    this.getGameById(this.gameId);
+  }
+
+  getEventsByGameId(gameId: number) {
+    this.eventService.getEventsByGameId(gameId).subscribe(
+      response => {
+        this.events = response;
+      },
+      error => {
+        this.error = error;
+      }
+    );
+  }
+
+  getGameById(gameId: number) {
+    this.gameService.getGameById(gameId).subscribe(
+      response => {
+        this.game = response;
+      },
+      error => {
+        this.error = error;
+      }
+    );
   }
 
 }
