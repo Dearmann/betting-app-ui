3641b6205f4e54b565d70b0ba00554396c6b7992
Dearmann <duszak0@gmail.com>
2022-11-29 02:53:41 +0100

    Bet | Comment | Rating services

---
+++ src/app/services/bet.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Bet } from '../model/bet';
+import { BetRequest } from '../model/bet-request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BetService {
+
+  private readonly backendUrl = "http://localhost:8080/bets";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllBets(): Observable<Bet[]> {
+    return this.httpClient.get<Bet[]>(this.backendUrl);
+  }
+
+  getBetById(betId: number): Observable<Bet> {
+    return this.httpClient.get<Bet>(this.backendUrl + "/" + betId);
+  }
+
+  getAllBetsByUserId(userId: string): Observable<Bet> {
+    return this.httpClient.get<Bet>(this.backendUrl + "/by-userid/" + userId);
+  }
+
+  getAllBetsByMatchId(matchId: number): Observable<Bet> {
+    return this.httpClient.get<Bet>(this.backendUrl + "/by-matchid/" + matchId);
+  }
+
+  createBet(betRequest: BetRequest): Observable<Bet> {
+    return this.httpClient.post<Bet>(this.backendUrl, betRequest);
+  }
+
+  editBet(betRequest: BetRequest, betId: number): Observable<Bet> {
+    return this.httpClient.put<Bet>(this.backendUrl + "/" + betId, betRequest);
+  }
+
+  deleteBet(betId: number): Observable<void> {
+    return this.httpClient.delete<void>(this.backendUrl + "/" + betId);
+  }
+
+  createRequestDto(userId: string, matchId: number, predictedTeamId: number): BetRequest {
+    return {
+      "userId": userId, 
+      "matchId": matchId, 
+      "predictedTeamId": predictedTeamId, 
+    }
+  }
+
+}
---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comment } from '../model/comment';
+import { CommentRequest } from '../model/comment-request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  private readonly backendUrl = "http://localhost:8080/comments";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllComments(): Observable<Comment[]> {
+    return this.httpClient.get<Comment[]>(this.backendUrl);
+  }
+
+  getCommentById(commentId: number): Observable<Comment> {
+    return this.httpClient.get<Comment>(this.backendUrl + "/" + commentId);
+  }
+
+  getAllCommentsByUserId(userId: string): Observable<Comment> {
+    return this.httpClient.get<Comment>(this.backendUrl + "/by-userid/" + userId);
+  }
+
+  getAllCommentsByMatchId(matchId: number): Observable<Comment> {
+    return this.httpClient.get<Comment>(this.backendUrl + "/by-matchid/" + matchId);
+  }
+
+  createComment(commentRequest: CommentRequest): Observable<Comment> {
+    return this.httpClient.post<Comment>(this.backendUrl, commentRequest);
+  }
+
+  editComment(commentRequest: CommentRequest, commentId: number): Observable<Comment> {
+    return this.httpClient.put<Comment>(this.backendUrl + "/" + commentId, commentRequest);
+  }
+
+  deleteComment(commentId: number): Observable<void> {
+    return this.httpClient.delete<void>(this.backendUrl + "/" + commentId);
+  }
+
+  createRequestDto(userId: string, matchId: number, message: string): CommentRequest {
+    return {
+      "userId": userId, 
+      "matchId": matchId, 
+      "message": message, 
+    }
+  }
+
+}
