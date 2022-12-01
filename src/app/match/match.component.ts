fad4be6f0decc3dee044dcb65b03e99e6a7b335d
Dearmann <duszak0@gmail.com>
2022-12-01 18:57:44 +0100

    Create comment

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Bet } from '../model/bet';
 import { Match } from '../model/match';
 import { Rating } from '../model/rating';
+import { CommentService } from '../services/comment.service';
 import { MatchService } from '../services/match.service';
 import { RatingService } from '../services/rating.service';
 import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-match',
@@ -16,17 +19,24 @@ export class MatchComponent implements OnInit {
 
   matchId: number;
   match!: Match;
+  userId: string;
+
   averageRating: number = 0;
   betPercentageTeam1: number = 0;
   betPercentageTeam2: number = 0;
+
+  messageControl: FormControl = new FormControl('');
 
   constructor(
     private readonly matchService: MatchService,
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly snackbarService: SnackbarService,
-    private readonly ratingService: RatingService
+    private readonly userService: UserService,
+    private readonly ratingService: RatingService,
+    private readonly commentService: CommentService
     ) {
+      this.userId = this.userService.userProfile?.id!;
       this.matchId = parseInt(this.route.snapshot.paramMap.get('matchId') || "");
       if (!this.matchId) {
         router.navigateByUrl("");
@@ -68,7 +78,11 @@ export class MatchComponent implements OnInit {
   }
 
   createComment() {
-
+    const commentRequestDto = this.commentService.createRequestDto(this.userId, this.matchId, this.messageControl.value);
+    this.commentService.createComment(commentRequestDto).subscribe({
+      error: response => this.snackbarService.showError(response, 'Failed to create comment'),
+      complete: () => this.snackbarService.showSuccess('Comment created successfully')
+    });
   }
 
 }
