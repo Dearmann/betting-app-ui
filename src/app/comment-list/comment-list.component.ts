838c299ab16d56dbb212a50a4cde26fc38f1d468
Dearmann <duszak0@gmail.com>
2022-12-01 13:33:49 +0100

    Delete comment

--- src/app/comment-list/comment-list.component.ts
+++ src/app/comment-list/comment-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogConfirmDeleteComponent } from '../admin-panel/dialog-confirm-delete/dialog-confirm-delete.component';
 import { Comment } from "../model/comment";
+import { CommentService } from '../services/comment.service';
+import { SnackbarService } from '../services/snackbar.service';
 
 @Component({
   selector: 'app-comment-list',
@@ -8,19 +12,49 @@ import { Comment } from "../model/comment";
 })
 export class CommentListComponent implements OnInit {
 
-  @Input() comments!: Comment[];
+  @Input() matchId: number = 0;
+  comments!: Comment[];
 
-  constructor() { }
+  constructor(
+    private readonly commentService: CommentService,
+    private readonly dialog: MatDialog,
+    private readonly snackbarService: SnackbarService
+    ) { }
 
   ngOnInit(): void {
+    this.getAllCommentsByMatchId(this.matchId);
   }
 
-  editComment(commentId: number) {
+  getAllCommentsByMatchId(matchId: number) {
+    this.commentService.getAllCommentsByMatchId(matchId).subscribe({
+      next: response => this.comments = response,
+      error: response => this.snackbarService.showError(response, 'Failed to retrieve comments')
+    })
+  }
+
+  editButtonClicked(commentId: number) {
     console.log(commentId);
+  }
+
+  deleteButtonClicked(commentId: number) {
+    let deleteDialog = this.dialog.open(DialogConfirmDeleteComponent, {
+      data: { name: 'comment' }
+    });
+    deleteDialog.afterClosed().subscribe(confirmation => {
+      if (confirmation) {
+        this.deleteComment(commentId);
+      }
+    });
   }
 
   deleteComment(commentId: number) {
-    console.log(commentId);
+    this.commentService.deleteComment(commentId).subscribe({
+      error: response => this.snackbarService.showError(response, 'Failed to delete comment'),
+      complete: () => {
+        this.getAllCommentsByMatchId(this.matchId);
+        this.snackbarService.showSuccess('Comment deleted successfully');
+      }
+    })
   }
 
 }
